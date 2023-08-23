323d6391a5cedb4dc42f5b93dfde1be440f70e80
Vincenzo.Veniero <vincenzo.veniero@MBPdiVioVeniero.homenet.telecomitalia.it>
2023-08-23 15:07:16 +0200

    data_extractor generated

---
+++ data_extractor/src/libs/ReportGenerator.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+class ReportGenerator {
+
+  constructor() {}
+
+  async generateReport( outputPath, env, result, format ) {
+    const timestamp = new Date().toISOString() 
+    switch (format) {
+      case 'csv':
+        if(result.hasOwnProperty("logs")) {
+          await this._generateCSVLogFileResult( outputPath, timestamp, env, result.logs, "logs" );
+        }
+        if(result.hasOwnProperty("trace_ids")){
+          if(result?.trace_ids){
+            await this._generateJSONLogFileResult( outputPath, timestamp, env, result.trace_ids, "trace_ids" );
+          }
+          console.log("N° " + result.trace_ids["to_review"].length + " trace ids to review.")
+        }
+        break;
+      case 'json':
+        await this._generateJSONLogFileResult( outputPath, timestamp, env, result, "output" );
+        break;
+    }
+  }
+
+
+  async _generateCSVLogFileResult( outputPath, timestamp, env, result, filename ) {
+    const folder = outputPath + "/" + env + timestamp 
+    fs.mkdirSync(folder, { recursive: true })
+    const csvContent = [];
+    csvContent.push(Object.keys(result[0]).join(';')); // Intestazione
+
+    result.forEach((item) => {
+        const values = Object.values(item).map(value => `"${value}"`);
+        csvContent.push(values.join(';'));
+    });
+
+    // Convertire il contenuto CSV in una stringa
+    const csvString = csvContent.join('\n');
+
+    // Scrivere la stringa CSV in un file
+    fs.writeFileSync(folder + '/' + filename + '.csv', csvString, 'utf-8');
+
+    console.log('File CSV ' + filename + '.csv creato con successo.');
+  }
+
+  async _generateJSONLogFileResult( outputPath, timestamp, env, result, filename ) {
+    const folder = outputPath + "/" + env + timestamp 
+    fs.mkdirSync(folder, { recursive: true })
+    fs.writeFileSync(folder+'/'+filename+'.json', JSON.stringify(result, null, 4), 'utf-8')
+    console.log('File JSON '+filename + '.json creato con successo.');
+  }
+}
+
+
+
+
+exports.ReportGenerator = ReportGenerator;
