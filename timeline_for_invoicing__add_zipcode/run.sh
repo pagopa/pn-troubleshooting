9496a14398b5047e29b0788b2f4b7063cc478891
marco-vit-pagopa <marco.vit@pagopa.it>
2023-10-11 03:47:13 +0200

    PN-8204 fix paths

--- timeline_for_invoicing__add_zipcode/run.sh
+++ timeline_for_invoicing__add_zipcode/run.sh
@@ -10,9 +10,9 @@ rm -rf ./out/TABLE_NAME_*
 
 jshell --class-path ./libs/jettison-1.5.4.jar:./classes src/java/Script.java \
     "-R-Xmx2g" \
-    "-R-Darg1=/Users/mvit/Documents/pn/pn-troubleshooting/bi_or_not_bi/rstudio-server/workspace/data/ConfinfoCdc/dump/data" \
-    "-R-Darg2=/Users/mvit/Documents/pn/pn-troubleshooting/bi_or_not_bi/rstudio-server/workspace/data/cdc/TABLE_NAME_pn-TimelinesForInvoicing" \
-    "-R-Darg3=/Users/mvit/Documents/pn/pn-troubleshooting/timeline_for_invoicing__add_zipcode/out/TABLE_NAME_pn-TimelinesForInvoicing"
+    "-R-Darg1=input_data/conf_obj/data" \
+    "-R-Darg2=input_data/TABLE_NAME_pn-TimelinesForInvoicing" \
+    "-R-Darg3=./out/TABLE_NAME_pn-TimelinesForInvoicing"
 
 echo "Ended with exit code $?"
 
