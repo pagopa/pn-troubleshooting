ab868f0d08f9bd6915ae6887cbd765d5f57b9030
Vincenzo.Veniero <vincenzo.veniero@Host-007.homenet.telecomitalia.it>
2023-12-01 14:49:16 +0100

    get paper error

---
+++ get_pnPaperError/README.md
@@ -0,0 +1,32 @@
+# get_pnPaperError
+
+Script di Scan di una tabella DynamoDB.
+
+## Tabella dei Contenuti
+
+- [Descrizione](#descrizione)
+- [Installazione](#installazione)
+- [Utilizzo](#utilizzo)
+
+## Descrizione
+
+Lo Script, data in input il nome di tabella DynamoDB e dopo aver configurato i parametri all'interno del codice effettua una scan che restituisce i risultati in base alle condizioni inserite.
+
+## Installazione
+
+```bash
+npm install
+```
+
+## Utilizzo
+
+```bash
+node scan_dynamo.js --awsProfile <aws-profile> --tableName <dynamodb-table> --filter <filter>
+```
+Dove:
+- `<aws-profile>` è il profilo dell'account AWS dell'ambiente di riferimento.
+- `<dynamodb-table>` é la tabella sulla quale si intende effettuare una scan.
+- `<filter>` inserire un valore casual al momento
+
+## Output
+Lo script genera un file in formato JSON contenente i requestIDx estratti
---
+++ get_pnPaperError/index.js
@@ -0,0 +1,72 @@
+const { AwsClientsWrapper } = require("./libs/AwsClientWrapper");
+const { parseArgs } = require('util');
+const fs = require('fs');
+
+
+function _checkingParameters(args, values){
+  const usage = "Usage: node index.js --awsProfile <aws-profile> --tableName <table-name> --filter <filter>"
+  //CHECKING PARAMETER
+  args.forEach(el => {
+    if(el.mandatory && !values.values[el.name]){
+      console.log("Param " + el.name + " is not defined")
+      console.log(usage)
+      process.exit(1)
+    }
+  })
+  args.filter(el=> {
+    return el.subcommand.length > 0
+  }).forEach(el => {
+    if(values.values[el.name]) {
+      el.subcommand.forEach(val => {
+        if (!values.values[val]) {
+          console.log("SubParam " + val + " is not defined")
+          console.log(usage)
+          process.exit(1)
+        }
+      })
+    }
+  })
+}
+
+async function _writeInFile(result, filename ) {
+  fs.mkdirSync("result", { recursive: true });
+  fs.writeFileSync('result/' + filename+'.json', JSON.stringify(result, null, 4), 'utf-8')
+}
+
+async function main() {
+
+  const args = [
+    { name: "awsProfile", mandatory: true, subcommand: [] },
+    { name: "tableName", mandatory: true, subcommand: [] },
+    { name: "filter", mandatory: true, subcommand: [] },
+  ]
+  const values = {
+    values: { awsProfile, tableName, filter },
+  } = parseArgs({
+    options: {
+      awsProfile: {
+        type: "string", short: "p", default: undefined
+      },
+      tableName: {
+        type: "string", short: "t", default: undefined
+      },
+      filter: {
+        type: "string", short: "f", default: undefined
+      },
+    },
+  });  
+
+  _checkingParameters(args, values)
+  const awsClient = new AwsClientsWrapper( awsProfile );
+  
+  const res = await awsClient._scanRequest(tableName);
+  const requestIdx = []
+  res.forEach(element => {
+    console.log(element)
+  }); 
+  _writeInFile(res, "RequestIDx")
+  console.log(res);
+
+}
+
+main();
