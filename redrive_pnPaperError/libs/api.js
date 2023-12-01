9e4ad891a6c746a4c623993e5631c8125853cec6
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-12-01 14:58:27 +0100

    Submit pn paper error

---
+++ redrive_pnPaperError/libs/api.js
@@ -0,0 +1,16 @@
+
+async function sendNationalRegistriesRequest(){
+
+}
+
+// with PF or PG prefix
+async function decodeUID(uid){
+
+}
+
+const ApiClient = {
+    sendNationalRegistriesRequest,
+    decodeUID
+}
+
+exports.ApiClient = ApiClient;
