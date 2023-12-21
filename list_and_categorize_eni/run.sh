cb0547343236d7ee383c9444d73c95c2f9ad400e
mbrachi <matteo.brachi@pagopa.it>
2023-12-21 08:19:47 +0100

    modify script ip classification for hotfix env

--- list_and_categorize_eni/run.sh
+++ list_and_categorize_eni/run.sh
@@ -7,20 +7,20 @@ metadata_file="metadata.json"
 output_file="ip_classification.txt"
 
 echo "List current IPs"
-# aws --profile sso_pn-confinfo-prod --region eu-south-1\
-#     ec2 describe-network-interfaces \
-#     --no-paginate \
-#   | jq '.NetworkInterfaces | .[] ' \
-#   | jq ' { "account":"confinfo", "grpId": .Groups[0].GroupId, "grpName": .Groups[0].GroupName, "desc": .Description, "az": .AvailabilityZone,  "privIp": .PrivateIpAddress, "pubIp": .Association.PublicIp }' \
-#   | jq -r '. | tojson' | sort > confinfo.log
+ aws --profile sso_pn-confinfo-hotfix --region eu-south-1\
+     ec2 describe-network-interfaces \
+     --no-paginate \
+   | jq '.NetworkInterfaces | .[] ' \
+   | jq ' { "account":"confinfo", "grpId": .Groups[0].GroupId, "grpName": .Groups[0].GroupName, "desc": .Description, "az": .AvailabilityZone,  "privIp": .PrivateIpAddress, "pubIp": .Association.PublicIp }' \
+   | jq -r '. | tojson' | sort > confinfo.log
 
 
-# aws --profile sso_pn-core-prod --region eu-south-1\
-#     ec2 describe-network-interfaces \
-#     --no-paginate \
-#   | jq '.NetworkInterfaces | .[] ' \
-#   | jq ' { "account":"core", "grpId": .Groups[0].GroupId, "grpName": .Groups[0].GroupName, "desc": .Description, "az": .AvailabilityZone,  "privIp": .PrivateIpAddress, "pubIp": .Association.PublicIp }' \
-#   | jq -r '. | tojson' | sort > core.log
+ aws --profile sso_pn-core-hotfix --region eu-south-1\
+     ec2 describe-network-interfaces \
+     --no-paginate \
+   | jq '.NetworkInterfaces | .[] ' \
+   | jq ' { "account":"core", "grpId": .Groups[0].GroupId, "grpName": .Groups[0].GroupName, "desc": .Description, "az": .AvailabilityZone,  "privIp": .PrivateIpAddress, "pubIp": .Association.PublicIp }' \
+   | jq -r '. | tojson' | sort > core.log
 
 
 metadata_length=$( cat ${metadata_file} | jq -r '. | length' )
