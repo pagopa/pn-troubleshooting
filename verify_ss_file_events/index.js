cb585b8068a09ed10d398245ee115784d16f07b9
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-01-18 16:29:59 +0100

    PN-9637: verify_ss_file_events

---
+++ verify_ss_file_events/index.js
@@ -0,0 +1,83 @@
+const { AwsClientsWrapper } = require("./libs/AwsClientWrapper");
+const { parseArgs } = require('util');
+const fs = require('fs');
+
+function _checkingParameters(args, values){
+  const usage = "Usage: index.js --envName <envName> --fileName <fileName>"
+  //CHECKING PARAMETER
+  args.forEach(el => {
+    if(el.mandatory && !values.values[el.name]){
+      console.log("Param " + el.name + " is not defined")
+      console.log(usage)
+      process.exit(1)
+    }
+  })
+  args.filter(el=> {
+    return el.subcommand.length > 0
+  }).forEach(el => {
+    if(values.values[el.name]) {
+      el.subcommand.forEach(val => {
+        if (!values.values[val]) {
+          console.log("SubParam " + val + " is not defined")
+          console.log(usage)
+          process.exit(1)
+        }
+      })
+    }
+  })
+}
+
+async function _writeInFile(result) {
+  fs.mkdirSync("result", { recursive: true });
+  const dateIsoString = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-');
+  fs.writeFileSync('result/result.json', JSON.stringify(result, null, 4), 'utf-8')
+}
+
+async function main() {
+
+  const args = [
+    { name: "envName", mandatory: true, subcommand: [] },
+    { name: "fileName", mandatory: true, subcommand: [] },
+  ]
+  const values = {
+    values: { envName, fileName, timing },
+  } = parseArgs({
+    options: {
+      envName: {
+        type: "string", short: "e", default: undefined
+      },
+      fileName: {
+        type: "string", short: "f", default: undefined
+      },
+    },
+  });  
+
+  _checkingParameters(args, values)
+  const awsClient = new AwsClientsWrapper( envName );
+  const filesKey = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' }).split('\n');
+  const listBuckets = await awsClient._getBucketLists();
+  const usefullBucket = listBuckets.Buckets.filter((x) => x.Name.indexOf("safestorage")>0);
+  for(file of filesKey) {
+    console.log("Verify " + file)
+    var temp = {}
+    for(bucket of usefullBucket) {
+      try {
+        res = await awsClient._getHeadObject(bucket.Name, file)
+        temp[bucket.Name] = res['$metadata'].httpStatusCode
+      }
+      catch (e) {
+        temp[bucket.Name] = e['$metadata'].httpStatusCode
+      }
+    }
+    Object.keys(temp).forEach(b => {
+      if(((b.indexOf('staging') > 0 && temp[b] == 404 ) || temp[b] == 200)) {
+        return;
+      }
+      else {
+        console.log("Error! " + file)
+      }
+    })
+  }
+}
+
+main();
---
+++ verify_ss_file_events/libs/AwsClientWrapper.js
@@ -0,0 +1,57 @@
+
+const { fromIni } = require("@aws-sdk/credential-provider-ini");
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
+const { S3Client, HeadObjectCommand, ListBucketsCommand } = require("@aws-sdk/client-s3");
+
+function awsClientCfg( profile ) {
+  const self = this;
+  //if(!profileName){
+    return { 
+      region: "eu-south-1", 
+      credentials: fromIni({ 
+        profile: profile,
+      })
+    }
+  //}
+}
+
+class AwsClientsWrapper {
+
+  constructor( envName, profileName, roleArn ) {
+    const ssoProfile = `sso_pn-confinfo-${envName}`
+    this._dynamoClient = new DynamoDBClient( awsClientCfg( ssoProfile, profileName, roleArn ));
+    this._s3Client = new S3Client( awsClientCfg( ssoProfile, profileName, roleArn ));
+  }
+
+  async _queryRequest(tableName, key, value, projection){
+    const input = { // QueryInput
+      TableName: tableName, // required
+      ProjectionExpression: projection,
+      KeyConditionExpression: "#k = :k",
+      ExpressionAttributeNames: { // ExpressionAttributeNameMap
+        "#k": key,
+      },
+      ExpressionAttributeValues: {
+        ":k": { "S": value }
+      },
+    };
+    const command = new QueryCommand(input);
+    return await this._dynamoClient.send(command);
+  }
+
+  async _getHeadObject(bucketName, key){
+    const input = { // HeadObjectRequest
+      Bucket: bucketName, // required
+      Key: key, // required
+    };
+    const command = new HeadObjectCommand(input);
+    return await this._s3Client.send(command);
+  }
+
+  async _getBucketLists(){
+    const command = new ListBucketsCommand({});
+    return await this._s3Client.send(command);
+  }
+}
+
+exports.AwsClientsWrapper = AwsClientsWrapper;
