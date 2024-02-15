fcd3d60a404129b6a39c8e5de2e694a6029f10e4
Christian La Riccia <christian.lariccia@pagopa.it>
2024-02-15 15:39:52 +0100

    feat(pn-9966): add partition by remediaton action
    Now processing output is divided in multiple partitions depending on distinct remediation action

--- bi_analog_analysis/src/utils/custom_data_frame_writer.py
+++ bi_analog_analysis/src/utils/custom_data_frame_writer.py
@@ -1,7 +1,6 @@
 import os.path
 import uuid
-
-from typing import Optional, NoReturn
+from typing import Optional, NoReturn, List
 
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.readwriter import DataFrameWriter
@@ -17,7 +16,8 @@ class CustomDataFrameWriter:
             output_name: str,
             output_folder: str,
             output_format: Optional[str] = Format.PARQUET.value,
-            mode: Optional[str] = 'overwrite'
+            mode: Optional[str] = 'overwrite',
+            partition_by: Optional[str | List[str]] = None
     ) -> NoReturn:
         """
 
@@ -26,6 +26,7 @@ class CustomDataFrameWriter:
         :param output_folder:       Destination folder
         :param output_format:       Format of the destination file , default is 'parquet'
         :param mode:                Mode for writing operation, default is 'overwrite'
+        :param partition_by:        The Column to partition by, default is None which means no partitioning
         """
 
         # Ensure unique file export
@@ -34,7 +35,18 @@ class CustomDataFrameWriter:
         output_path: str = os.path.join(output_folder, unique_uuid, output_name) + '.' + output_format
         dataframe_writer: DataFrameWriter = df.write.mode(saveMode=mode)
 
+        if partition_by is not None:
+            dataframe_writer.partitionBy(partition_by)
+
         if output_format == Format.CSV.value:
-            dataframe_writer.options(header='True', delimiter=';').csv(output_path)
+            CustomDataFrameWriter._write_csv(dataframe_writer=dataframe_writer, output_path=output_path)
         else:
-            dataframe_writer.parquet(output_path)
+            CustomDataFrameWriter._write_parquet(dataframe_writer=dataframe_writer, output_path=output_path)
+
+    @staticmethod
+    def _write_csv(dataframe_writer: DataFrameWriter, output_path: str) -> None:
+        dataframe_writer.options(header='True', delimiter=';').csv(output_path)
+
+    @staticmethod
+    def _write_parquet(dataframe_writer: DataFrameWriter, output_path: str) -> None:
+        dataframe_writer.parquet(output_path)
