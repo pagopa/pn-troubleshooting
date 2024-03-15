2f835d80ab805529013ecf042d3357d5db5cd900
marco-vit-pagopa <marco.vit@pagopa.it>
2024-03-15 15:55:01 +0100

    Wip script di port forwarding

--- bi_or_not_bi__ec_metadata/prepare_forward.sh
+++ bi_or_not_bi__ec_metadata/prepare_forward.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
 
-AWS_PROFILE=sso_pn-core-prod-rw
+AWS_PROFILE=cons_admin_try_spark
 AWS_REGION=eu-south-1
 
 
@@ -11,21 +11,20 @@ spark_ec2_instance_id=$( aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
     --output text --query 'Reservations[*].Instances[*].InstanceId' )
 
 aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
-    ssm start-session \
-    --target "${spark_ec2_instance_id}" \
-    --document-name AWS-StartPortForwardingSessionToRemoteHost \
-    --parameters '{"portNumber":["4040"],"localPortNumber":["4040"],"host":["127.0.0.1"]}' &
+    ec2 start-instances \
+    --instance-ids "${spark_ec2_instance_id}" 
 
 aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
     ssm start-session \
     --target "${spark_ec2_instance_id}" \
-    --document-name AWS-StartPortForwardingSessionToRemoteHost \
-    --parameters '{"portNumber":["10100"],"localPortNumber":["10100"],"host":["127.0.0.1"]}' &
-
-
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["4040"],"localPortNumber":["4040"]}' &
 
 aws --profile ${AWS_PROFILE} --region ${AWS_REGION} \
     ssm start-session \
     --target "${spark_ec2_instance_id}" \
-    --document-name AWS-StartPortForwardingSessionToRemoteHost \
-    --parameters '{"portNumber":["443"],"localPortNumber":["5601"],"host":["vpc-opensearchservi-bqkt8cyjx7kv-5onk33bdpiheq52de5plhfzx5i.eu-south-1.es.amazonaws.com"]}'
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["10100"],"localPortNumber":["10100"]}' &
+
+
+
