b637bd6a8642bd8472b7c1d4ef761340637d5abf
Christian La Riccia <christian.lariccia@pagopa.it>
2024-05-03 18:05:08 +0200

    fix(pn-10656): add AssumedRoleCredentialProvider credential provider

--- bi_or_not_bi__ec_metadata/src/main/java/it/pagopa/pn/scripts/commands/reports/ShipperReliabilityReportCommand.java
+++ bi_or_not_bi__ec_metadata/src/main/java/it/pagopa/pn/scripts/commands/reports/ShipperReliabilityReportCommand.java
@@ -55,7 +55,7 @@ public class ShipperReliabilityReportCommand implements Callable<Integer> {
         SparkConf sparkConf = new SparkConf()
             .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
             .set("spark.hadoop.fs.s3a.path.style.access", "true")
-            .set("spark.hadoop.fs.s3a.requester.pays.enabled", "true");
+            .set("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.auth.AssumedRoleCredentialProvider");
 
         SparkSqlWrapper spark = SparkSqlWrapper.local(APPLICATION_NAME, sparkConf, true);
         spark.addListener(logger);
