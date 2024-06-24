aaa1ad74e998cc2677a06b74012af0ec6a6110aa
ElenaPagnacco <116713610+ElenaPagnacco@users.noreply.github.com>
2024-06-24 13:03:06 +0200

    PN-11664: changed separator, inverted date check, removed suffix _2 on output files, removed logs.

--- ss-coherence-check/index.js
+++ ss-coherence-check/index.js
@@ -104,7 +104,7 @@ async function processLines(path) {
       })
       .catch((error) => {
         failedItems++;
-        fs.appendFileSync("failures_2.txt", line + "," + error + "," + new Date(Date.now()).toISOString() + "\r\n");
+        fs.appendFileSync("failures.txt", line + ";" + error + ";" + new Date(Date.now()).toISOString() + "\r\n");
       })
       .finally(() => {
         progressBar.update(readItems);
@@ -114,70 +114,51 @@ async function processLines(path) {
 
 async function processLine(line) {
   // Esegui operazioni asincrone sulla riga
-  console.log("Processing key ", line);
   let s3CheckSum;
   // recupero record da Dynamo
   const record = await dynamoDbService.getItem("pn-SsDocumenti", line);
-  console.log("Dynamo record: \r\n", record);
   // recupero l'oggetto s3
   const object = await s3Service.getObject(bucket, line);
-  console.log("S3 object: \r\n", object);
   // trasformo l'oggetto s3 in bytearray
   let objBA = await object.Body.transformToByteArray();
 
   // ricavo checkSum con cui hashare oggetto s3
   let hashType = record.documentType.checksum;
-  console.log("Hashing with: ", hashType);
 
   // verifico che hashing sia disponibile
   if (hashType != "NONE") {
     s3CheckSum = hashObject(hashType, objBA);
   } else {
-    fs.appendFileSync("incoherent_2.txt", line + ",hashtype not available," + new Date(Date.now()).toISOString() + "\r\n");
+    fs.appendFileSync("incoherent.txt", line + ";hashtype not available;" + new Date(Date.now()).toISOString() + "\r\n");
   }
-
-  /*// ATTENZIONE: record.contentLenght fa riferimento alla property Dynamo in ambiente dev_confinfo, in caso di undefined controllare che typo persista
-  console.log("record size: ", record.contentLenght);*/
 
   // verifico coerenza dell'hashing e dimensioni del file
   if (record.checkSum == s3CheckSum && record.contentLenght == object.ContentLength) {
-    // console.log("document state:" ,record.documentState);
     if (record.documentState.toUpperCase() == "DELETED" || record.documentState.toUpperCase() == "ATTACHED") {
       //calcolo la data equivalente a oggi+120gg
       let in120Days = new Date();
       in120Days.setDate((in120Days.getDate() + 120));
       in120Days.setMilliseconds(0);
-      // verifico che la retention del documento sia superiore a una settimana a partire da now()
-      /*console.log("retention until: ", new Date(record.retentionUntil));
-      console.log("in 120 days: ", in120Days);
-      console.log("retention <= in120Days: ", (new Date(record.retentionUntil) <= in120Days));*/
-      if (new Date(record.retentionUntil) <= in120Days) {
-        fs.appendFileSync("incoherent_2.txt", line + ",expiration date in less than 120 days: " + record.retentionUntil + "," + new Date(Date.now()).toISOString() + "\r\n");
+      if (new Date(record.retentionUntil) >= in120Days) {
+        fs.appendFileSync("incoherent.txt", line + ";expiration date in more than 120 days: " + record.retentionUntil + ";" + new Date(Date.now()).toISOString() + "\r\n");
       } else {
-        fs.appendFileSync("output_2.txt", line + "\r\n");
+        fs.appendFileSync("output.txt", line + "\r\n");
       }
-
     } else {
-      fs.appendFileSync("incoherent_2.txt", line + ",incoherent status: " + record.documentState.toUpperCase() + "," + new Date(Date.now()).toISOString() + "\r\n");
+      fs.appendFileSync("incoherent.txt", line + ";incoherent status: " + record.documentState.toUpperCase() + ";" + new Date(Date.now()).toISOString() + "\r\n");
     }
   } else {
-    fs.appendFileSync("incoherent_2.txt", line + ",incoherent size or checksum," + new Date(Date.now()).toISOString() + "\r\n");
+    fs.appendFileSync("incoherent.txt", line + ";incoherent size or checksum;" + new Date(Date.now()).toISOString() + "\r\n");
   }
 }
 
 
 // hashing dell'oggetto s3
 function hashObject(hashType, objBA) {
-  console.log("Starting file hashing...")
-
   const hash = crypto.createHash(hashType);
-  const format = 'base64';
-
   hash.update(objBA);
-  s3CheckSum = hash.digest(format);
-  console.log("File hashing done!")
+  s3CheckSum = hash.digest('base64');
   return s3CheckSum;
-
 }
 
 // Metodo principale
@@ -191,7 +172,7 @@ async function main() {
 function logFinalReport() {
   console.log(`Ending process at ${new Date(Date.now()).toISOString()}`)
   console.log(`Total items : ${totalItemCount}, Read items: ${readItems}, Worked items: ${workedItems}. Failures : ${failedItems}.`);
-  console.log(`Check "failures_2.txt" file for individual failures.`)
+  console.log(`Check "failures.txt" file for individual failures.`)
 }
 
 // Esecuzione del metodo principale
