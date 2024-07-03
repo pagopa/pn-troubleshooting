d830c73323fc550c9822371cbbcad2b46eb05e56
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-07-03 17:02:10 +0200

    pn-commons integration

--- pn-common/libs/dynamoUtil.js
+++ pn-common/libs/dynamoUtil.js
@@ -8,33 +8,44 @@ function prepareKeys(data) {
     return keys;
   }
   
-  function prepareExpressionAttributeNames(data) {
-    let expressionAttributeNames = {};
-    Object.keys(data).forEach(k => {
-        expressionAttributeNames[data[k]['codeAttr']] = k
-    });
-    return expressionAttributeNames;
-  }
+function prepareExpressionAttributeNames(data) {
+  let expressionAttributeNames = {};
+  Object.keys(data).forEach(k => {
+      expressionAttributeNames[data[k]['codeAttr']] = k
+  });
+  return expressionAttributeNames;
+}
 
-  function prepareExpressionAttributeValues(data) {
-    let expressionAttributeValues = {};
-    Object.keys(data).forEach(k => {
-      expressionAttributeValues[data[k]['codeValue']] = marshall(data[k]['value'])
-    });
-    return expressionAttributeValues;
-  }
-  
-  function prepareUpdateExpression(operator, data) {
-    let updateExpression = `${operator} `
-    Object.keys(data).forEach(k => {
-      updateExpression = `${updateExpression} ${data[k]['codeAttr']} = ${data[k]['codeValue']},`
-    });
-    return updateExpression.substring(0, updateExpression.length - 1)
-  }
-  
-  module.exports = { 
-    prepareKeys, 
-    prepareExpressionAttributeNames,
-    prepareExpressionAttributeValues, 
-    prepareUpdateExpression 
+function prepareExpressionAttributeValues(data) {
+  let expressionAttributeValues = {};
+  Object.keys(data).forEach(k => {
+    expressionAttributeValues[data[k]['codeValue']] = marshall(data[k]['value'])
+  });
+  return expressionAttributeValues;
+}
+
+function prepareUpdateExpression(operator, data) {
+  let updateExpression = `${operator} `
+  Object.keys(data).forEach(k => {
+    updateExpression = `${updateExpression} ${data[k]['codeAttr']} = ${data[k]['codeValue']},`
+  });
+  return updateExpression.substring(0, updateExpression.length - 1)
+}
+
+function prepareKeyConditionExpression(data, logicalOperator) {
+  let conditionExpression = '' 
+  Object.keys(data).forEach(k => {
+    if(data[k]['operator']) {
+      conditionExpression = `${conditionExpression} ${data[k]['codeAttr']} ${data[k]['operator']} ${data[k]['codeValue']} ${logicalOperator}`
+    }
+  });
+  return conditionExpression.substring(0, conditionExpression.length -  logicalOperator.length)
+}
+
+module.exports = { 
+  prepareKeys, 
+  prepareExpressionAttributeNames,
+  prepareExpressionAttributeValues, 
+  prepareUpdateExpression,
+  prepareKeyConditionExpression
 };
