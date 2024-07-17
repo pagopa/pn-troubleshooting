75e1d09a1812483a6f5359935d6e0ee54526ab2a
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-07-17 10:58:24 +0200

    PN-12103: verify_analog_filekey_order script

--- pn-common/libs/utils.js
+++ pn-common/libs/utils.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
 async function sleep( ms ) {
     return new Promise( ( accept, reject) => {
       setTimeout( () => accept(null) , ms );
@@ -12,8 +15,29 @@ function _getAttemptFromRequestId(requestId) {
   return requestId.split("ATTEMPT_")[1][0];
 }
 
+function _parseCSV(fileName, delimiter) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    const parser = fs.createReadStream(fileName).pipe(parse({
+      columns: true, 
+      delimiter: delimiter, 
+      trim: true
+    }));
+    parser.on('data', (data) => {
+      results.push(data)
+    });
+    parser.on('error', (err) => {
+      reject(err);
+    });
+    parser.on('end', () => {
+      resolve(results);
+    });
+  })
+}
+
 module.exports = { 
   sleep,
   _getIunFromRequestId,
-  _getAttemptFromRequestId
+  _getAttemptFromRequestId,
+  _parseCSV
 };
