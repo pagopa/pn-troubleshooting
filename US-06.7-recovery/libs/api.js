b6e40cef497bf47a34708e80b48c0ab855ab0777
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-07-25 09:29:35 +0200

    PN-11727: fix

--- US-06.7-recovery/libs/api.js
+++ US-06.7-recovery/libs/api.js
@@ -1,16 +1,13 @@
 const axios = require('axios');
 const https = require('https');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 const agent = new https.Agent({  
-  rejectUnauthorized: false
+  rejectUnauthorized: process.env.AGENT
 });
 
 
 async function requestToOpensearch(data){
     let url = process.env.BASE_URL+'/pn-logs*/_search';
-    console.log(url)
-    console.log(JSON.stringify(data))
     let response = await axios.post(url, data, {
         auth: {
           username: process.env.USERNAME,
@@ -18,7 +15,6 @@ async function requestToOpensearch(data){
         },
         httpsAgent: agent 
       });
-    console.log(response.data)
     return response.data;
 }
 
