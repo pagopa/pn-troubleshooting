8cd3df5644ee51bbe35893b8ef4f4f1161223300
Martina Mattei <82142676+martiMattei@users.noreply.github.com>
2024-07-29 14:29:49 +0200

    Bug/pn 12153 (#150)
    * PN-12153 - script per report su requestId con insertTimestamp null e statusDateTime non in ordine cronologico
    
    * PN-12153 - fix filterExpression for paperProgrStatus null
    
    * PN-12153 - clean
    
    * PN-12153 - fix filterExpression for paperProgrStatus not null
    
    * PN-12153 - fix filterExpression for paperProgrStatus not null
    
    * PN-12153 - update records
    
    * PN-12153 - fix README.md e client region
    
    * PN-12153 - fix progressBar and filterExpression
    
    * PN-12153 - fix progressBar count
    
    * PN-12153 - fix scan condition
    
    * PN-12153 - add version param on update
    
    * PN-12153 - fix updateInsertTimestamp with sort eventsList
    
    * PN-12153: added deprecated notice
    
    * PN-12153: added second deprecation notice
    
    ---------
    
    Co-authored-by: Mario Ottone <mario.ottone@dgsspa.com>
    Co-authored-by: mottone-dgs <119047198+mottone-dgs@users.noreply.github.com>

--- request-metadata-cleaning/README.md
+++ request-metadata-cleaning/README.md
@@ -11,9 +11,26 @@ Script per la bonifica dei dati contenuti nella tabella pnEc-RichiesteMetadati.
 
 ## Descrizione
 
-Il package contiene uno script atto alla bonifica dei requestMetadata.
+Il package contiene quattro script atti alla bonifica dei requestMetadata.
+
+### index.js
 La bonifica consiste nell'inserire in ogni record il nuovo attributo "lastUpdateTimestamp"
 e in ogni evento della lista "eventsList" l'attributo "insertTimestamp".
+
+### lastUpdateTimestamp-future.js
+La bonifica consiste nella verifica del valore dell'attributo "lastUpdateTimestamp" di ogni record.
+Se il valore è impostato nel futuro, viene aggiornato con la data ora corrente.
+
+### data-sort-clean.js
+La bonifica consiste nel verificare la presenza di "insertTimestamp" all'interno di ogni evento di ogni record. 
+Se il valore è NULL, il campo viene aggiornato con il massimo timestamp trovato con anno 1970 e aggiunto +1 ai millis. 
+Se il valore è NULL e non vi sono presenti altri timestamp valorizzati, il record verrà aggiornato inserendo come data
+la data di inizio epoche.
+Se viene eseguito l'aggiornamento, gli eventi vengono ordinati cronologicamente secondo il valore di "insertTimestamp".
+L'ordinamento può anche essere eseguito indipendentemente dall'esecuzione dell'aggiornamento.
+
+### data-setup.js
+Script atto alla creazione di dati appositi per i test.
 
 ## Installazione
 
@@ -29,11 +46,14 @@ npm install
 aws sso login --profile sso_pn-confinfo-<env>
 ```
 
-### Esecuzione
+### index.js
 
 ```bash
 node index.js --awsProfile <aws-profile> --exclusiveStartKey <exclusive-start-key> --scanLimit <scan-limit> --requestIdsPath <request-ids-path> --test --dryrun
 ```
+
+**DEPRECATED**
+Questo script è stato utilizzato nel primo run della bonifica, ma non va utilizzato in quanto presenta un problema nell'aggiornamento di eventi non correttamente ordinati e ne salva una nuova copia ordinata quando non dovrebbe
 
 Dove:
 
@@ -56,6 +76,60 @@ Dove:
 Se è attiva la modalità test, verrà anche generato un file _"test-records.csv"_ contenente i requestId dei record
 che sono stati aggiornati.
 
+#### lastUpdateTimestamp-future.js
+
+```bash
+node lastUpdateTimestamp-future.js --awsProfile <aws-profile> --exclusiveStartKey <exclusive-start-key> --scanLimit <scan-limit> --requestIdsPath <request-ids-path> --test --dryrun
+```
+
+Dove:
+
+- `<aws-profile>` è il profilo dell'account AWS. Se non viene inserito, verranno prese di default le credenziali AWS di
+  sistema; `OPZIONALE`
+- `<exclusive-start-key>` settare questo parametro permette di cominciare la prima scan a partire dalla primary key
+  indicata. Se non inserito, la prima scan partirà dall'inizio della tabella; `OPZIONALE`
+- `<scan-limit>` è il numero massimo di record reperibili da una singola scan. Come default, vengono reperiti record
+  fino a che non viene raggiunta la soglia massima (definita da AWS) di 1MB per singola scan. `OPZIONALE`
+- `<request-ids-path>` indica il path di un file contenente delle requestId. Se inserito, attiva il metadata cleaning
+  SOLO per le requestId indicate. Questa modalità non prevede scan della tabella. `OPZIONALE`
+- `<test>` se inserito, attiva la modalità test. In questa modalità, viene eseguita una singola scan di 10 record dalla
+  tabella. `OPZIONALE`
+- `<dryrun>` se inserito, attiva la modalità dryrun. Questa modalità attiva automaticamente anche quella di test, e in
+  piu'
+  disattiva le operazioni di scrittura. `OPZIONALE`
+  Alla fine del processo di bonifica, verrà generato un file _"failures.csv"_ contenente i requestId dei record
+  su cui l'update è andato in eccezione e la causa dell'errore.
+
+
+Se è attiva la modalià dryrun, verrà generato un file _"dryrun-updated.csv"_ contenente i requestId dei record che sarebbero stati aggiornati.
+Altrimenti, se è attiva la modalità test, verrà generato un file _"test-updated.csv"_ contenente i requestId dei record
+che sono stati aggiornati.
+Se nessuna delle due modalità è attiva, verrà generato un file _"updated.csv"_ contenente i requestId dei record che sono stati aggiornati.
+
+### data-sort-clean.js
+
+```bash
+node data-sort-clean.js --awsProfile <aws-profile> --scanLimit <scanLimit> --updateInsertTimestamp --updateEventOrder --test --dryrun
+```
+
+Dove:
+
+- `<awsProfile>` è il profilo dell'account AWS. Se non viene inserito, verranno prese di default le credenziali AWS di
+  sistema; `OPZIONALE`
+- `<scanLimit>` è il parametro che indica il numero di record presi in considerazione in ogni scan; `OPZIONALE`
+- `<updateInsertTimestamp>` è il parametro che indica se deve essere fatto o meno l'aggiornamento; `OPZIONALE`
+- `<updateEventOrder>` **DEPRECATED** è il parametro che permette l'ordinamento degli eventi senza prima fare l'aggiornamento; è stato implementato, ma non essendo più necessario non sono stati eseguiti test esaustivi; `OPZIONALE`
+- `<test>` se inserito, attiva la modalità test. In questa modalità, viene eseguita una singola scan di 10 record dalla
+  tabella. `OPZIONALE`
+- `<dryrun>` se inserito, attiva la modalità dryrun. Questa modalità attiva automaticamente anche quella di test, e 
+disattiva le operazioni di scrittura. `OPZIONALE`
+
+- Lo script genera un file "output_requestId-missing-insertTimestamp.txt" in cui vengono inserite le sole requestId che 
+hanno almeno un insertTimestamp non valorizzato.
+Lo script genera anche un file "output_requestId-insertTimestamp_disorder.txt" in cui vengono elencate le requestId che 
+non hanno gli eventi in ordine cronologico.
+In caso di errori, verrà generato un file "failures.csv".
+
 ## Testing
 
 Il package contiene anche uno script di setup per creare dei record appositi per i test.
@@ -64,10 +138,10 @@ Il package contiene anche uno script di setup per creare dei record appositi per
 node data-setup.js --awsProfile <aws-profile> --digitalRecordsNum <num1 num2 num3> --paperRecordsNum <num1 num2 num3>
 ```
 
-- `awsProfile` è il profilo dell'account AWS. Se non viene inserito, verranno prese di default le credenziali AWS di
+- `<awsProfile>` è il profilo dell'account AWS. Se non viene inserito, verranno prese di default le credenziali AWS di
   sistema; `OPZIONALE`
-- `digitalRecordsNum` è un array di numeri che indica il numero di request digitali da creare per ogni tipologia di entry.
-- `paperRecordsNum` è un array di numeri che indica il numero di request cartacee da creare per ogni tipologia di entry.
+- `<digitalRecordsNum>` è un array di numeri che indica il numero di request digitali da creare per ogni tipologia di entry.
+- `<paperRecordsNum>` è un array di numeri che indica il numero di request cartacee da creare per ogni tipologia di entry.
 
 Le tipologie di entry sono, in ordine di posizionamento nell'array, queste tre:
 
@@ -78,10 +152,3 @@ Le tipologie di entry sono, in ordine di posizionamento nell'array, queste tre:
 
 Il package contiene anche uno script che genera due file txt in cui vengono riportate le sole requestId, per tutti quei 
 record che hanno insertTimestamp non valorizzato e statusDateTime degli eventi non in ordine cronologico.
-
-```bash
-node data-sort-clean.js --awsProfile <aws-profile> --scanLimit <scanLimit>
-```
-- `awsProfile` è il profilo dell'account AWS. Se non viene inserito, verranno prese di default le credenziali AWS di
-  sistema; `OPZIONALE`
-- `scanLimit` è il parametro che indica il numero di record presi in considerazione in ogni scan; `OPZIONALE`
--- request-metadata-cleaning/data-sort-clean.js
+++ request-metadata-cleaning/data-sort-clean.js
@@ -1,172 +1,336 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
 const { fromSSO } = require("@aws-sdk/credential-provider-sso");
 const fs = require('fs');
-const { parseArgs } = require('util');
 const process = require('node:process');
+const { DynamoDBClient, DescribeTableCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+const { StandardRetryStrategy } = require("@smithy/middleware-retry");
+const { parseArgs } = require('util');
+const cliProgress = require('cli-progress');
 
-class DynamoDBScanner {
-  constructor(params) {
-    this.tableName = params.tableName || 'pn-EcRichiesteMetadati';
-    this.region = params.region || 'eu-south-1';
-    this.scanLimit = params.scanLimit ? parseInt(params.scanLimit, 10) : undefined;
-    this.outputFilePath1 = 'output_requestId-missing-insertTimestamp.txt';
-    this.outputFilePath2 = 'output_requestId-statusDateTime_disorder.txt';
+const progressBar = new cliProgress.SingleBar({
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+  hideCursor: true,
+  noTTYOutput: true
+});
 
-    this.awsProfile = params.awsProfile;
-    this.dryrun = params.dryrun || false;
-    this.test = params.test || false;
+console.log(`Starting process at ${new Date(Date.now()).toISOString()}`);
 
-    this.credentials = this.awsProfile ? fromSSO({ profile: this.awsProfile })() : undefined;
+const args = [
+  { name: "awsProfile", mandatory: false },
+  { name: "scanLimit", mandatory: false },
+  { name: "test", mandatory: false },
+  { name: "dryrun", mandatory: false },
+  { name: "updateInsertTimestamp", mandatory: false },
+  { name: "updateEventOrder", mandatory: false },
+];
 
-    this.dynamoDbClient = new DynamoDBClient({
-      credentials: this.credentials,
-      region: this.region,
-    });
-
-    this.dynamoDbDocumentClient = DynamoDBDocumentClient.from(this.dynamoDbClient);
-
-    this.fileStream1 = this.createFileStream(this.outputFilePath1);
-    this.fileStream2 = this.createFileStream(this.outputFilePath2);
-  }
-
-  createFileStream(filePath) {
-    const fileStream = fs.createWriteStream(filePath, { flags: 'a' });
-    fileStream.on('error', (err) => {
-      console.error(`Errore durante la scrittura su ${filePath}: ${err}`);
-    });
-    return fileStream;
-  }
-
-  async scanTable(params, processItem) {
-    let lastEvaluatedKey = null;
-    let scannedCount = 0;
-    let isScanning = true;
-
-    while (isScanning) {
-      try {
-        const data = await this.dynamoDbDocumentClient.send(new ScanCommand(params));
-        const newItems = data.Items || [];
-
-        console.log(`Scanned ${data.ScannedCount} items`);
-
-        newItems.forEach(item => processItem(item));
-
-        scannedCount += newItems.length;
-
-        if (this.scanLimit && scannedCount >= this.scanLimit) {
-          console.log('Limite di scansione raggiunto.');
-          isScanning = false;
-        }
-
-        lastEvaluatedKey = data.LastEvaluatedKey;
-        if (lastEvaluatedKey) {
-          params.ExclusiveStartKey = lastEvaluatedKey;
-        } else {
-          console.log('Nessun LastEvaluatedKey, terminando la scansione.');
-          isScanning = false;
-        }
-      } catch (err) {
-        console.error('Errore durante la scansione della tabella:', err);
-        isScanning = false;
-      }
-    }
-  }
-
-  async scanAndProcessItems() {
-    const params = {
-      TableName: this.tableName,
-      ProjectionExpression: "requestId, eventsList",
-      FilterExpression: "attribute_exists(eventsList) AND attribute_exists(eventsList[0].paperProgrStatus) AND NOT attribute_type(eventsList[0].paperProgrStatus, :nullType)",
-          ExpressionAttributeValues: {
-            ":nullType": "NULL"
-          },
-      Limit: this.scanLimit,
-    };
-
-    const processItem = item => {
-      const requestId = item.requestId;
-      const eventsList = item.eventsList || [];
-      let statusDateTimes = [];
-      let missingInsertTimestamp = false;
-
-      eventsList.forEach(event => {
-        if (event.paperProgrStatus && typeof event.paperProgrStatus === 'object') {
-          const status = event.paperProgrStatus;
-          if (status && status.statusDateTime) {
-            const date = new Date(status.statusDateTime);
-            statusDateTimes.push(date);
-          }
-        }
-
-        if (!event.insertTimestamp) {
-          missingInsertTimestamp = true;
-        }
-      });
-
-      if (!this.isInChronologicalOrder(statusDateTimes)) {
-        const outputLine = `${requestId}\n`;
-        this.fileStream2.write(outputLine);
-      }
-
-      if (missingInsertTimestamp) {
-        const outputLine = `${requestId}\n`;
-        this.fileStream1.write(outputLine);
-      }
-    };
-
-    await this.scanTable(params, processItem);
-  }
-
-  isInChronologicalOrder(dates) {
-    for (let i = 1; i < dates.length; i++) {
-      if (dates[i] < dates[i - 1]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  async run() {
-    try {
-      await this.scanAndProcessItems();
-      console.log('File di output generati.');
-    } catch (error) {
-      console.error(`Errore nel processo: ${error}`);
-    } finally {
-      this.closeFileStreams();
-    }
-  }
-
-  closeFileStreams() {
-    this.fileStream1.end();
-
-    this.fileStream2.end();
-  }
-}
-
-const args = parseArgs({
+const { values } = parseArgs({
   options: {
     awsProfile: { type: "string" },
     scanLimit: { type: "string" },
     test: { type: "boolean" },
     dryrun: { type: "boolean" },
-    tableName: { type: "string" },
-    region: { type: "string" }
+    updateInsertTimestamp: { type: "boolean" },
+    updateEventOrder: { type: "boolean" },
   },
 });
 
-const scanner = new DynamoDBScanner(args.values);
+const { awsProfile, scanLimit, test, dryrun, updateInsertTimestamp, updateEventOrder } = values;
+let scanLimitParsed = parseInt(scanLimit) || null;
+if (test) { scanLimitParsed = 10; }
 
-scanner.run()
-  .then(() => console.log('Processo completato con successo.'))
-  .catch(error => console.error(`Errore nel processo: ${error}`));
+var confinfoCredentials;
+if (awsProfile != null) { confinfoCredentials = fromSSO({ profile: awsProfile })(); }
+
+const customRetryDecider = (err) => {
+  console.log("Retrying for exception: " + err);
+  return true;
+};
+
+const MAXIMUM_ATTEMPTS = 3;
+const DELAY_RATIO = 3000;
+
+const retryStrategy = new StandardRetryStrategy(
+  () => Promise.resolve(MAXIMUM_ATTEMPTS),
+  {
+    delayDecider: (_delayBase, attempts) => DELAY_RATIO * attempts,
+    retryDecider: customRetryDecider,
+  },
+);
+retryStrategy.mode = 'STANDARD';
+
+const dynamoDbClient = new DynamoDBClient({
+  credentials: confinfoCredentials,
+  region: 'eu-south-1',
+  retryStrategy: retryStrategy
+});
+const dynamoDbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
+const tableName = "pn-EcRichiesteMetadati";
+const outputFilePath1 = 'output_requestId-missing-insertTimestamp.txt';
+const outputFilePath2 = 'output_requestId-insertTimestamp_disorder.txt';
+
+const fileStream1 = createFileStream(outputFilePath1);
+const fileStream2 = createFileStream(outputFilePath2);
+
+function createFileStream(filePath) {
+  const fileStream = fs.createWriteStream(filePath, { flags: 'a' });
+  fileStream.on('error', (err) => {
+    console.error(`Error writing to ${filePath}: ${err}`);
+  });
+  return fileStream;
+}
+
+let totalItems = 0;
+let itemUpdates = 0;
+let itemFailures = 0;
+let scannedCount = 0;
+
+async function getTotalRecords() {
+    var response = await dynamoDbClient.send(new DescribeTableCommand({ TableName: tableName }));
+    return response.Table.ItemCount;
+  }
+
+async function scanTable(params, processItem) {
+  let lastEvaluatedKey = null;
+  let isScanning = true;
+
+  try {
+    totalItems = await getTotalRecords();
+    progressBar.start(totalItems, 0);
+  } catch (err) {
+    console.error('\nError counting items in table:', err);
+    return;
+  }
+
+  while (isScanning) {
+    try {
+      if (lastEvaluatedKey) {
+        params.ExclusiveStartKey = lastEvaluatedKey;
+      }
+
+      const data = await dynamoDbDocumentClient.send(new ScanCommand(params));
+      const newItems = data.Items || [];
+
+      for (const item of newItems) {
+        await processItem(item);
+
+
+      }
+      scannedCount += data.ScannedCount;
+      progressBar.update(scannedCount);
+
+      lastEvaluatedKey = data.LastEvaluatedKey;
+      if (!lastEvaluatedKey) {
+        isScanning = false;
+        console.log('\nScan complete.');
+      }
+    } catch (err) {
+      console.error('\nError scanning table:', err);
+      isScanning = false;
+    }
+  }
+
+  progressBar.stop();
+}
+
+// Check se le date sono in ordine cronologico
+function isInChronologicalOrder(dates) {
+  for (let i = 1; i < dates.length; i++) {
+    if (dates[i] < dates[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Scan and process items dalla tabella
+async function scanAndProcessItems() {
+  const params = {
+    TableName: tableName,
+    ProjectionExpression: "requestId, eventsList, version",
+    FilterExpression: "attribute_exists(eventsList)",
+    Limit: scanLimitParsed,
+    ConsistentRead: true,
+  };
+
+
+  const processItem = async item => {
+    const requestId = item.requestId;
+    const eventsList = item.eventsList || [];
+    let insertTimestamps = [];
+    let missingInsertTimestamp = false;
+    let version = item.version;
+
+    eventsList.forEach(event => {
+   if (event.insertTimestamp) {
+           const date = new Date(event.insertTimestamp);
+           insertTimestamps.push(date);
+         } else {
+           missingInsertTimestamp = true;
+         }
+    });
+
+
+    if (!isInChronologicalOrder(insertTimestamps)) {
+      const outputLine = `${requestId}\n`;
+      fileStream2.write(outputLine);
+        if(updateEventOrder){
+        var newSortedEventsList = sortEventsByInsertTimestamp(eventsList);
+        await updateRecordEventListOrdered(requestId, newSortedEventsList, version);
+        }
+    }
+
+    if (missingInsertTimestamp) {
+      const outputLine = `${requestId}\n`;
+      fileStream1.write(outputLine);
+        if(updateInsertTimestamp){
+        await updateRecordInsertTimestamp(requestId, eventsList, version);
+        }
+    }
+
+  };
+
+  await scanTable(params, processItem);
+}
+
+// Get max insertTimestamp degli eventi con anno 1970
+function getMaxInsertTimestamp(eventsList) {
+  let maxTimestamp = new Date(0); // Inizializzazione con epoch time
+  eventsList.forEach(event => {
+    if (event.insertTimestamp) {
+      const timestamp = new Date(event.insertTimestamp);
+      if (timestamp.getFullYear() === 1970 && timestamp > maxTimestamp) {
+        maxTimestamp = timestamp;
+      }
+    }
+  });
+  return maxTimestamp;
+}
+
+// Ordinamento eventi
+function sortEventsByInsertTimestamp(eventsList) {
+  return eventsList.sort((a, b) => {
+    const aTime = a.insertTimestamp ? new Date(a.insertTimestamp).getTime() : 0;
+    const bTime = b.insertTimestamp ? new Date(b.insertTimestamp).getTime() : 0;
+    return aTime - bTime;
+  });
+}
+
+// Update di insertTimestamp
+async function updateRecordInsertTimestamp(requestId, eventsList, currentVersion) {
+    try {
+        const maxInsertTimestamp1970 = getMaxInsertTimestamp(eventsList);
+        let newInsertTimestamp = new Date(maxInsertTimestamp1970.getTime() + 1).toISOString();
+
+        const updatedEventsList = eventsList.map(event => {
+          if (!event.insertTimestamp) {
+            event.insertTimestamp = newInsertTimestamp;
+               newInsertTimestamp = new Date(new Date(newInsertTimestamp).getTime() + 1).toISOString();          }
+          return event;
+        });
+
+        const sortedEventsList = sortEventsByInsertTimestamp(updatedEventsList);
+
+        const updateParams = {
+          TableName: tableName,
+          Key: { requestId },
+          ExpressionAttributeNames: {
+            "#eventsList": "eventsList",
+            '#version': 'version'
+          },
+          ConditionExpression: "#version = :expectedVersion",
+          UpdateExpression: 'SET #eventsList = :eventsList, #version = :newVersion',
+          ExpressionAttributeValues: {
+            ':eventsList': sortedEventsList,
+            ':expectedVersion': currentVersion,
+            ':newVersion': currentVersion + 1,
+          }
+        };
+
+        if (!dryrun) {
+          try {
+            await dynamoDbDocumentClient.send(new UpdateCommand(updateParams));
+            itemUpdates++;
+          } catch (err) {
+            if (err.name === 'ConditionalCheckFailedException') {
+              console.error(`Conditional check failed for record ${requestId}: ${err}`);
+            } else {
+              console.error(`Failed to update record ${requestId}: ${err}`);
+            }
+            itemFailures++;
+          }
+        }
+      } catch (err) {
+        console.error(`Failed to update record ${requestId}: ${err}`);
+        itemFailures++;
+      }
+}
+
+// Update degli eventi ordinati
+async function updateRecordEventListOrdered(requestId, sortedEventsList, currentVersion) {
+  try {
+    const updateParams = {
+      TableName: tableName,
+      Key: { requestId },
+      ExpressionAttributeNames: {
+        "#eventsList": "eventsList",
+        '#version': 'version'
+      },
+
+      ConditionExpression: "#version = :expectedVersion",
+      UpdateExpression: 'SET #eventsList = :eventsList, #version = :newVersion',
+
+      ExpressionAttributeValues: {
+        ':eventsList': sortedEventsList,
+        ':expectedVersion': currentVersion,
+        ':newVersion': currentVersion + 1,
+      }
+    };
+
+    if (!dryrun) {
+
+      await dynamoDbDocumentClient.send(new UpdateCommand(updateParams));
+      itemUpdates++;
+    }
+  } catch (err) {
+    console.error(`Failed to update event list for record ${requestId}: ${err}`);
+    itemFailures++;
+  }
+}
+
+async function run() {
+  try {
+    await scanAndProcessItems();
+    console.log('Output files created.');
+  } catch (error) {
+    console.error(`Errore nel processo: ${error}`);
+  } finally {
+    closeFileStreams();
+    logFinalReport();
+  }
+}
+
+function closeFileStreams() {
+  fileStream1.end();
+  fileStream2.end();
+}
+
+function logFinalReport() {
+  console.log(`Ending process at ${new Date(Date.now()).toISOString()}`);
+  console.log(`Scanned items: ${scannedCount}.`);
+  console.log(`Updated items: ${itemUpdates}.`);
+  console.log(`Failed updates: ${itemFailures}.`);
+}
 
 function handleProcessSignal(signal) {
-  console.log(`Received ${signal} signal. Ending script execution.`);
+  console.log(`\nReceived ${signal} signal. Ending script execution.`);
+  logFinalReport();
   process.exit();
 }
 
-process.on('SIGINT', handleProcessSignal);
-process.on('SIGTERM', handleProcessSignal);
-process.on('SIGHUP', handleProcessSignal);
+process.on('SIGINT', () => handleProcessSignal('SIGINT'));
+process.on('SIGTERM', () => handleProcessSignal('SIGTERM'));
+process.on('SIGHUP', () => handleProcessSignal('SIGHUP'));
+
+run();
