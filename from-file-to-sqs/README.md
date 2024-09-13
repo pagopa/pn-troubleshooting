a9f095d176dac26b68db647f4e54f5a59561c4a7
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2024-09-13 10:49:23 +0200

    add import-file-to-sqs

---
+++ from-file-to-sqs/README.md
@@ -0,0 +1,37 @@
+# temporary_fix_automation_script_dp-inputs
+
+script che inserisce in una coda SQS (standard o fifo) degli elementi a partire da un file
+
+## Tabella dei Contenuti
+
+- [Descrizione](#descrizione)
+- [Installazione](#installazione)
+- [Utilizzo](#utilizzo)
+
+## Descrizione
+
+script che inserisce in coda degli elementi a partire da un file
+
+## Installazione
+
+```bash
+npm install
+```
+
+## Utilizzo
+### Step preliminare
+
+```bash
+aws sso login --profile sso_pn-core-<env>
+```
+
+### Esecuzione
+```bash
+node index.js --envName <env-name> --fileName <file-name> --outputQueue <output-queue> [--dryrun]
+
+```
+Dove:
+- `<env-name>` l'environment sul quale si intende avviare lo script
+- `<file-name>` file contenente la lista dei requestId
+- `<output-queue>` nome della coda SQS su cui inviare gli eventi. Può essere una coda standard o fifo.
+- `<dryrun>` per eseguire in readonly mode
---
+++ from-file-to-sqs/index.js
@@ -0,0 +1,95 @@
+const { parseArgs } = require('util');
+const fs = require('fs');
+const path = require('path');
+const { AwsClientsWrapper } = require("pn-common");
+const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const crypto = require('crypto');
+
+function prepareMessageAttributes(attributes) {
+  let att = {}
+  if(attributes != null) {
+    Object.keys(attributes).forEach(k => {
+      att[k] = attributes[k]
+    });
+  }
+  return att;
+}
+
+function _checkingParameters(args, values){
+  const usage = "Usage: node index.js --envName <env-name> --fileName <file-name> --outputQueue <output-queue>] [--dryrun]"
+  //CHECKING PARAMETER
+  args.forEach(el => {
+    if(el.mandatory && !values.values[el.name]){
+      console.log("Param " + el.name + " is not defined")
+      console.log(usage)
+      process.exit(1)
+    }
+  })
+  args.filter(el=> {
+    return el.subcommand.length > 0
+  }).forEach(el => {
+    if(values.values[el.name]) {
+      el.subcommand.forEach(val => {
+        if (!values.values[val]) {
+          console.log("SubParam " + val + " is not defined")
+          console.log(usage)
+          process.exit(1)
+        }
+      })
+    }
+  })
+}
+
+async function main() {
+
+  const args = [
+    { name: "envName", mandatory: true, subcommand: [] },
+    { name: "fileName", mandatory: true, subcommand: [] },
+    { name: "outputQueue", mandatory: true, subcommand: [] },
+    { name: "dryrun", mandatory: false, subcommand: [] },
+
+  ]
+  const values = {
+    values: { envName, fileName, outputQueue, dryrun },
+  } = parseArgs({
+    options: {
+      envName: {
+        type: "string", short: "e", default: undefined
+      },
+      fileName: {
+        type: "string", short: "f", default: undefined
+      },
+      outputQueue: {
+        type: "string", short: "q", default: undefined
+      },
+      dryrun: {
+        type: "boolean", short: "n", default: false
+      },
+    },
+  });  
+  _checkingParameters(args, values)
+  const awsClient = new AwsClientsWrapper( 'core', envName );
+  awsClient._initSQS()
+  const queueUrl = await awsClient._getQueueUrl(outputQueue);
+  const fileRows = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' }).split('\n')
+  for(let i = 0; i < fileRows.length; i++){
+    const event = JSON.parse(fileRows[i])
+    const messageAttributes = prepareMessageAttributes(event.MessageAttributes)
+    let messageDeduplicationId
+    let messageGroupId
+    if(outputQueue.endsWith('fifo')) {
+      messageDeduplicationId = `${crypto.randomUUID()}-${i}`
+      messageGroupId = event.MessageAttributes.eventId.StringValue
+    }
+    if(!dryrun) {
+      const eventBody = JSON.parse(event.Body)
+      console.log(`Sending message`, eventBody)
+      await awsClient._sendSQSMessage(queueUrl, eventBody, 0, messageAttributes, messageGroupId, messageDeduplicationId)
+    }
+    else {
+      console.log(`DRYRUN: Sending message ${JSON.stringify(event.Body)}`)
+    }
+  }
+}
+
+main();
