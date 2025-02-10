607778d66140cd297aa491c8bf3790b0b5b701d9
gperric <gaspare.perricone@external.pagopa.it>
2025-02-10 19:47:47 +0100

    Implementata scrittura ouput su file; implementata logica controllo esistenza item; corretto README.md; implementato parsing parametri di input

--- update_ttl_pn-action/index.js
+++ update_ttl_pn-action/index.js
@@ -1,24 +1,73 @@
 const { AwsClientsWrapper } = require("pn-common");
-const { _parseCSV } = require("utils");
+const { _parseCSV } = require("../pn-common/libs/utils.js")
+const { parseArgs } = require('util');
+const { mkdirSync } = require('node:fs');
+const fs = require('node:fs/promises');
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+const path = require('path');
 
 // ------------ Parametri input ---------------------
+
+// --- Fissi ---
 const accountType = "core"; // 207905393513 account id pn-core-hotfix
-const env = "hotfix";
-//const fileName = "./core_hotfix_pn-Actions-notBefore2024.csv"
-const fileName = "./singleLine.csv"
+const tableName = "pn-Action";
+
+// --- Variabili ---
+const args = [
+    // { name: "account_type", mandatory: true}, // core
+    { name: "region", mandatory: false},
+    { name: "env", mandatory: true},
+    { name: "days", mandatory: true},
+    { name: "fileName", mandatory: true}
+  ];
+
+  const parsedArgs = { values: { region, env, days, fileName }} = parseArgs(
+      { options: {
+            // account_type: {type: "string",short: "a"},
+            region: {type: "string", short: "r", default: "eu-south-1"},
+            env: {type: "string",short: "e"},
+            days: {type: "string",short: "d"}, //
+            fileName: {type: "string",short: "f"}
+        }
+  });  
 
 // --------------------------------------------------
 
-// Inizializzazione client DynamoDB
-const dynDbClient = new AwsClientsWrapper(accountType,env);
-dynDbClient._initDynamoDB();
+function _checkingParameters(args, parsedArgs){
 
+    const usage = "Usage: node index.js [--region <region>]" +
+        " --env <env> --days <number> --fileName <csv file>\n";
+
+	// Verifica dei valori degli argomenti passati allo script
+	 function isOkValue(argName,value,ok_values){
+	     if(!ok_values.includes(value)) {
+	     	console.log("Error: \"" + value + "\" value for \"--" + argName +
+	        	"\" argument is not available, it must be in " + ok_values + "\n");
+	     	process.exit(1);
+	     }
+	 };
+
+    // Verifica se un argomento è stato inserito oppure inserito con valore vuoto
+	args.forEach(el => {
+	    if(el.mandatory && !parsedArgs.values[el.name]){
+	        console.log("\nParam \"" + el.name + "\" is not defined or empty.")
+	        console.log(usage)
+	        process.exit(1)
+	    }
+	 });
+	
+	// const account_types = ["core","confinfo"];
+	// isOkValue("account_type",account_type,account_types);
+
+	const envs = ["dev","test","hotfix","uat"];
+	isOkValue("env",env,envs);
+};
 
 function increaseFtuInput(val,pk,ftu,csvRow){ //ftu = fieldToUpdate
     const input = {};
     input.keys = { 
         [pk]: csvRow[pk] // valore della pk da csv 
-     // [sk]: skVal // valore della sk da csv
+     // [sk]: csvRow[sk] // valore della sk da csv
     }; 
     input.values = {
         [ftu]: {
@@ -31,33 +80,63 @@ function increaseFtuInput(val,pk,ftu,csvRow){ //ftu = fieldToUpdate
           keys                  --> { actionId: { S: 'valore actionId' } }
           Alias per ttl         --> { '#ttl': 'newttl' }
           Nuovo valore di TTL   -->{ ':newttl': { N: '<numero>' } }
-          Espressione nuovo ttl --> SET  #ttl = :newTtl
+          Espressione nuovo ttl --> SET  #ttl = :newttl
           */
         }
       };
     return input;
 };
 
-async function main() {
+function createOutputFile(folder) {
+    mkdirSync(path.join(__dirname, "results", folder), { recursive: true });
+    const dateIsoString = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-');
+    const resultPath = path.join(__dirname, "results", folder, 'updateItems_from_csv_' + dateIsoString + '.json');
+    return resultPath;
+    //fs.writeFileSync(resultPath, JSON.stringify(result, null, 4), 'utf-8')
+};
 
-    const tableName = "pn-Action";
+async function main() {
 
     // _parseCSV Scarta automaticamente gli header ed elimina gli ""
     const parsedCsv = await _parseCSV(fileName,","); // array di oggetti
     
+    // Qui potrei creare aggiungere un altro blocco try/catch
+    passedFileHandler = await fs.open(createOutputFile("passed"),'a'); // Se il file non esiste verrà creato
+    failedFileHandler = await fs.open(createOutputFile("failed"),'a'); // Se il file non esiste verrà creato
+    
     for(const row of parsedCsv) {
-        //ftu = fieldToUpdate
-        // Attributo da incrementare ----------------|
-        // PK --------------------------------|      |
-        // gg da aggiuntere ----------|       |      |     
-        let input = increaseFtuInput(1,"actionId","ttl",row);
+        // ftu = fieldToUpdate
+        // Attributo da incrementare -----------------|
+        // PK --------------------------------|       |
+        // gg da aggiungere ---------|        |       |     
+        let input = increaseFtuInput(days,"actionId","ttl",row);
         try {
-            const result = await dynDbClient._updateItem(tableName, input.keys, input.values,'SET');
-            console.log(result);
+            const { Attributes } = await dynDbClient._conditionalUpdateItem(tableName, input.keys, input.values,'SET',`attribute_exists(#ttl)`);
+            let returnedActionId = unmarshall(Attributes).actionId;
+            passedFileHandler.appendFile(returnedActionId + '\n');
+            console.log("OK: " + returnedActionId);
         } catch(e) {
-            console.log(e);
-        }
+            if (e.__type.includes("ConditionalCheckFailedException")){
+                console.log("WARN: Item with actionId '" + row.actionId + "' is empty");
+                failedFileHandler.appendFile(row.actionId + '\n');
+            } 
+            else {
+                console.log(e);
+                passedFileHandler?.close();
+                failedFileHandler?.close();
+                process.exit(1);
+            }
+        };
     };
+    passedFileHandler?.close();  
+    failedFileHandler?.close();
 };
+
+// Check dei parametri di input
+_checkingParameters(args,parsedArgs);
+
+// Inizializzazione client DynamoDB
+const dynDbClient = new AwsClientsWrapper(accountType,env);
+dynDbClient._initDynamoDB();
 
 main();
