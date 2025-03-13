5fec271bf26deac07e2f24d846b9ea3ecc3ba9e2
avettpp <alessio.vettori@external.pagopa.it>
2025-03-13 11:27:38 +0100

    ops/GO-77-NextCommit

--- put_msgs_to_SQS/index.js
+++ put_msgs_to_SQS/index.js
@@ -1,9 +1,10 @@
 //-------------------------------------------------
 
 const { AwsClientsWrapper } = require('pn-common');
-const { readFileSync, writeFileSync, mkdirSync, existsSync } = require('fs');
+const { readFileSync, mkdirSync, existsSync, appendFileSync } = require('fs');
 const { dirname } = require('path');
-const { parseArgs, inspect } = require('util');
+const { parseArgs } = require('util');
+const { randomUUID } = require('crypto');
 
 const VALID_ENVIRONMENTS = ['dev', 'uat', 'test', 'prod', 'hotfix'];
 const VALID_ACCOUNT = ['core', 'confinfo'];
@@ -72,7 +73,7 @@ function formatFile(inputFile) {
         return (jsonArray);
     } else {
         // Add commas between JSON objects and enclose everything in square brackets
-        const jsonInLine = '[' + fileRows.replace(/}\n{"Body"/g, '},\n{"Body"') + ']';
+        const jsonInLine = '[' + fileRows.replace(/}\n{/g, '},\n{') + ']';
         // Convert the JSON string to an array of JSON objects
         const jsonArray = JSON.parse(jsonInLine);
         return (jsonArray);
@@ -86,28 +87,15 @@ function formatFile(inputFile) {
  */
 function formatInput(msgsToRepublish) {
     let allBlocksOfMsgs = [];
-    let indx = 1;
     while (msgsToRepublish.length > 0) {
         let singleBlockOfMsgs = [];
         msgsToRepublish.slice(0, 10).forEach(json => {
-            if (json.MessageAttributes) {
-                singleBlockOfMsgs.push(
-                    {
-                        Id: 'msg' + indx,
-                        MessageBody: json.Body,
-                        MessageAttributes: json.MessageAttributes
-                    }
-                )
-                indx++;
-            } else {
-                singleBlockOfMsgs.push(
-                    {
-                        Id: 'msg' + indx,
-                        MessageBody: json.Body
-                    }
-                )
-                indx++;
-            }
+            let msg = {
+                Id: randomUUID(),
+                MessageBody: json.Body
+            };
+            json.MessageAttributes ? msg.MessageAttributes = json.MessageAttributes : null;
+            singleBlockOfMsgs.push(msg);
         });
         allBlocksOfMsgs.push(singleBlockOfMsgs);
         msgsToRepublish.splice(0, 10);
@@ -117,17 +105,19 @@ function formatInput(msgsToRepublish) {
 
 /**
  * Creating an output file on error
- * @param {string} outputFile - Target file path
+ * @param {string} envName - envName
+ * @param {string} outputFile - Target file name
  * @param {array} data - JSON data to write (message not write)
  */
-function writeInFile(outputFile, data) {
+function writeInFile(folderName, fileName, data) {
     // Create directory if it doesn't exist
-    const dir = dirname(outputFile);
+    const path = ('./result/' + folderName + '/' + fileName);
+    const dir = dirname(path);
     if (!existsSync(dir)) {
         mkdirSync(dir, { recursive: true });
     }
     // Writing error messages to file
-    writeFileSync(outputFile, JSON.stringify(data, null, 2), 'utf-8');
+    appendFileSync(path, JSON.stringify(data) + "\n", 'utf-8');
 };
 
 async function main() {
@@ -137,6 +127,8 @@ async function main() {
     // Initialize AWS client
     const clientSQS = new AwsClientsWrapper(accountType, envName);
     clientSQS._initSQS();
+    const timestamp = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-');
+    const folderName = timestamp + "_" + envName;
     // GetQueueUrlRequest
     const queueUrl = await clientSQS._getQueueUrl(queueName);
     // Process input file
@@ -144,42 +136,51 @@ async function main() {
     const totMsgs = msgsList.length;
     // Group msgs into blocks of 10 elements and store these blocks in an overall object
     const allBlocksOfMsgs = formatInput(msgsList);
-
-    console.log('\n');
-    console.log(inspect(allBlocksOfMsgs, { showHidden: false, depth: null, colors: true }));
-    console.log('\n');
-
     let run = 0;
-    let msgsResubmit = 0;
-    let result = [];
+    let msgsOK = 0;
+    let msgsKO = 0;
+    // Execute the batch
     for (block of allBlocksOfMsgs) {
         try {
-            result = await clientSQS._sendSQSMessageBatch(queueUrl, allBlocksOfMsgs[run]);
+            let result = await clientSQS._sendSQSMessageBatch(queueUrl, block);
             if (result.Failed) {
                 try {
-                    writeInFile('results/msg_not_resubmitted_' + queueName + '.json', result.Failed);
+                    // Create the output format
+                    result.Failed.forEach(json => {
+                        let failure = {
+                            Id: json.Id,
+                            Code: json.Code,
+                            Error: json.Message,
+                            Body: block.find(msgs => msgs.Id === json.Id)?.MessageBody,
+                            MessageAttributes: block.find(msgs => msgs.Id === json.Id)?.MessageAttributes
+                        }
+                        writeInFile(folderName, 'msg_not_resubmitted_' + queueName + '.json', failure);
+                    });
                     console.log('\nFailed resubmission of ' + result.Failed.length + ' messages\n');
+                    msgsKO = msgsKO + result.Failed.length;
+                    msgsOK = result.Successful ? msgsOK + result.Successful.length : msgsOK + 0;
                 } catch (err) {
-                    console.log('\n');
                     console.error('\nError creating JSON file:', err);
+                    msgsKO = msgsKO + result.Failed.length;
+                    msgsOK = result.Successful ? msgsOK + result.Successful.length : msgsOK + 0;
                 }
             }
             else {
-                let block = run + 1;
-                console.log('\nAll ' + allBlocksOfMsgs[run].length + ' msgs of block ' + block + ' have been successfully resubmit');
-                msgsResubmit = msgsResubmit + allBlocksOfMsgs[run].length;
+                console.log('\nAll ' + block.length + ' msgs of block ' + run + ' have been successfully resubmit');
+                msgsOK = msgsOK + result.Successful.length;
             }
         }
         catch (err) {
             console.log('\n');
-            console.error('\nError while deleting ' + allBlocksOfMsgs[run].length + ' msgs in the block ' + block, JSON.stringify(err, null, 2));
+            console.error('\nError while resubmit ' + block.length + ' msgs in the block ' + run, JSON.stringify(err, null, 2));
+            msgsKO = result.Failed ? msgsKO + result.Failed.length : msgsKO + 0;
+            msgsOK = result.Successful ? msgsOK + result.Successful.length : msgsOK + 0;
         }
         run++
     };
-    console.log('\n');
-    console.log('Number of runs performed --> ' + allBlocksOfMsgs.length + ' of ' + run);
-    console.log('\n');
-    console.log('Number of msgs resubmit --> ' + msgsResubmit + ' of ' + totMsgs);
+    console.log('\nNumber of msgs OK  --> ' + msgsOK);
+    console.log('Number of msgs KO  --> ' + msgsKO);
+    console.log('TOT number of msgs --> ' + totMsgs);
     console.log('\n');
 };
 
