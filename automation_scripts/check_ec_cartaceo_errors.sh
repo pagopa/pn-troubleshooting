79c8e838eae9d508831dcf199cf1f5c7e3c958e7
Enrico Lionello <enrico.lionello@external.pagopa.it>
2025-04-15 17:20:14 +0200

    refactor: check node scripts directories

--- automation_scripts/check_ec_cartaceo_errors.sh
+++ automation_scripts/check_ec_cartaceo_errors.sh
@@ -51,8 +51,12 @@ echo "Output directory: $(realpath "$OUTPUTDIR")"
 #############################################
 
 echo "Dumping SQS queue..."
+if [[ ! -d "$WORKDIR/dump_sqs" ]]; then
+    echo "Script directory $WORKDIR/dump_sqs does not exist. Exiting."
+    exit 1
+fi
 cd "$WORKDIR/dump_sqs"
-node dump_sqs.js --awsProfile sso_pn-confinfo-prod --queueName pn-ec-cartaceo-errori-queue-DLQ.fifo > /dev/null
+node dump_sqs.js --awsProfile sso_pn-confinfo-prod --queueName pn-ec-cartaceo-errori-queue-DLQ.fifo 1>/dev/null
 
 # Get the most recent dump file
 ORIGINAL_DUMP=$(find "$WORKDIR/dump_sqs/result" -type f -exec ls -t1 {} + | head -1)
@@ -65,15 +69,20 @@ echo "Dump file: $ORIGINAL_DUMP"
 #######################################################
 # Step 2: Extract requestIdx values from the dump     #
 #######################################################
+if [[ ! -d "$WORKDIR/check_status_request" ]]; then
+    echo "Script directory $WORKDIR/check_status_request does not exist. Exiting."
+    exit 1
+fi
 cd "$WORKDIR/check_status_request"
-REQUEST_IDS_LIST="./${ORIGINAL_DUMP%.json}_all_request_ids.txt"
-cat "$ORIGINAL_DUMP" | jq -r '.[] | .Body | fromjson | .requestIdx' > "$REQUEST_IDS_LIST"
+BASENAME=$(basename "${ORIGINAL_DUMP%.json}")
+REQUEST_IDS_LIST="./${BASENAME}_all_request_ids.txt"
+jq -r '.[] | .Body | fromjson | .requestIdx' "$ORIGINAL_DUMP" > "$REQUEST_IDS_LIST"
 echo "Extracted requestIdx values to: $WORKDIR/check_status_request/$REQUEST_IDS_LIST"
 
 #############################################################
 # Step 3: Check request status on pn-EcRichiesteMetadati    #
 #############################################################
-node index.js --envName prod --fileName "$REQUEST_IDS_LIST" > /dev/null
+node index.js --envName prod --fileName "$REQUEST_IDS_LIST" 1>/dev/null
 
 # Assume that the node script produces an error.json file in this folder.
 ERROR_JSON="error.json"
@@ -86,7 +95,7 @@ fi
 # Step 4: Extract requests in error status        #
 ###################################################
 ERROR_REQUEST_IDS_LIST="./${ORIGINAL_DUMP%.json}_error_request_ids.txt"
-cat "$ERROR_JSON" | jq -r '.requestId | sub("pn-cons-000~"; "")' > "$ERROR_REQUEST_IDS_LIST"
+jq -r '.requestId | sub("pn-cons-000~"; "")' "$ERROR_JSON" > "$ERROR_REQUEST_IDS_LIST"
 echo "Extracted error requestIds to: $WORKDIR/check_status_request/$ERROR_REQUEST_IDS_LIST"
 echo "Total requestIds in error status (not to remove): $(wc -l < "$ERROR_REQUEST_IDS_LIST")"
 
@@ -102,7 +111,7 @@ echo "Total messages in JSONLine dump: $(wc -l < "$JSONLINE_DUMP")"
 # Step 6: Filter out events from requests in error    #
 #######################################################
 FILTERED_DUMP="${ORIGINAL_DUMP%.json}_filtered.jsonline"
-grep -F -v -f "$ERROR_REQUEST_IDS_LIST" "$JSONLINE_DUMP" > "$FILTERED_DUMP"
+grep -x -F -v -f "$ERROR_REQUEST_IDS_LIST" "$JSONLINE_DUMP" > "$FILTERED_DUMP"
 echo "Filtered dump stored in: $FILTERED_DUMP"
 echo "Total messages in filtered dump (to remove): $(wc -l < "$FILTERED_DUMP")"
 
@@ -122,11 +131,15 @@ echo "Files copied to $OUTPUTDIR."
 
 if $PURGE; then
     echo "Purge option enabled. Proceeding to remove events from the SQS queue..."
+    if [[ ! -d "$WORKDIR/remove_from_sqs" ]]; then
+        echo "Script directory $WORKDIR/remove_from_sqs does not exist. Exiting."
+        exit 1
+    fi
     cd "$WORKDIR/remove_from_sqs"
     echo "Waiting for the visibility timeout to expire..."
     sleep 30
     echo "Purging events from the SQS queue..."
-    node index.js --account confinfo --envName prod --queueName pn-ec-cartaceo-errori-queue-DLQ.fifo --visibilityTimeout 30 --fileName "$FILTERED_DUMP" > /dev/null
+    node index.js --account confinfo --envName prod --queueName pn-ec-cartaceo-errori-queue-DLQ.fifo --visibilityTimeout 30 --fileName "$FILTERED_DUMP" 1>/dev/null
 fi
 
 echo "Process completed."
