a46f92f20c3615cb050a824a3839cbfeee365424
gperric <gaspare.perricone@external.pagopa.it>
2025-04-18 15:37:33 +0200

    Ripristino modifiche wrapper

--- pn-common/libs/AwsClientWrapper.js
+++ pn-common/libs/AwsClientWrapper.js
@@ -7,7 +7,7 @@ const { EventBridgeClient, EnableRuleCommand, DisableRuleCommand, ListRulesComma
 const { KMSClient, DecryptCommand, EncryptCommand, ListKeysCommand, GetKeyRotationStatusCommand, ListResourceTagsCommand, DescribeKeyCommand, RotateKeyOnDemandCommand } = require("@aws-sdk/client-kms");
 const { KinesisClient, GetRecordsCommand, GetShardIteratorCommand } = require("@aws-sdk/client-kinesis");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
-const { S3Client, GetObjectCommand, PutObjectCommand, ListObjectVersionsCommand, DeleteObjectCommand} = require("@aws-sdk/client-s3");
+const { S3Client, GetObjectCommand, PutObjectCommand, ListBucketsCommand, ListObjectVersionsCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { SQSClient, GetQueueUrlCommand, ReceiveMessageCommand, DeleteMessageCommand, SendMessageCommand, SendMessageBatchCommand, GetQueueAttributesCommand } = require("@aws-sdk/client-sqs");
 const { STSClient, GetCallerIdentityCommand } = require("@aws-sdk/client-sts");
 const { AthenaClient, StartQueryExecutionCommand, GetQueryExecutionCommand, GetQueryResultsCommand: AthenaGetQueryResultsCommand } = require("@aws-sdk/client-athena");
