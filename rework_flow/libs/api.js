503d217b103bfd9116c52a548db5e279ad77f767
Vincenzo Veniero <44098666+ViVeniero@users.noreply.github.com>
2026-07-09 14:12:13 +0200

    PN-20186: rework_flow script (#374)
    PN-20186: rework_flow script

---
+++ rework_flow/libs/api.js
@@ -0,0 +1,31 @@
+async function restartAttempt(iun, attemptId, recIndex, reason, task) {
+    let url = `${process.env.ALB_BASE_URL}/delivery-push-private/v1/notifications/${iun}/restart-attempt`;
+    let headers = {
+        'Content-Type': 'application/json'
+    };
+    let data = {
+        "attemptId": attemptId,
+        "recIndex": `RECINDEX_${recIndex}`,
+        "reason": reason,
+        "task": task
+    };
+
+    console.log(url, headers, data);
+    let response = await fetch(url, {
+        method: 'PUT',
+        headers: headers,
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+    }
+
+    return await response.json();
+}
+
+const ApiClient = {
+    restartAttempt
+}
+
+exports.ApiClient = ApiClient;
